5ea97ea091fb194696c6f26b975fbb6111a93296
Arthur <65594296+InTheDarkAlone@users.noreply.github.com>
2020-11-11 12:40:00 +0500

    Full Task 7

---
+++ Task/Task/SLList.cpp
@@ -0,0 +1,69 @@
+﻿#include "SLList.h"
+
+
+#include <iostream>
+#include <list>
+
+int SLList::getData() const
+{
+	return m_data;
+}
+
+void SLList::setData(int data) 
+{
+	m_data = data;
+}
+
+SLList* SLList::next() const
+{
+	return m_nextElem;
+}
+
+bool SLList::hasNext() const
+{
+	return m_nextElem != nullptr;
+}
+
+SLList* SLList::beginList(int data)
+{
+	SLList* next = new SLList();
+	next->setData(data);
+	return next;
+}
+
+SLList* SLList::addElement(int data)
+{
+	SLList* next = new SLList();
+	next->setData(data);
+	m_nextElem = next;
+	return m_nextElem;
+}
+
+void SLList::removeNext()
+{
+	if (m_nextElem == nullptr)
+		return;
+	else
+		m_nextElem->removeNext();
+	delete m_nextElem;
+	m_nextElem = nullptr;
+}
+
+SLList* SLList::FindByIndex(int index)
+{
+	SLList* curr = this;
+	int count = 1;
+	while (curr->hasNext())
+	{
+		count += 1;
+		curr = curr->next();
+	}
+
+	if (index > count)
+		return nullptr;
+	index = count - index;
+	curr = this;
+	for (int i = 0; i < index; i++)
+		curr = curr->next();
+	return curr;
+}
