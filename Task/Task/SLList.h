4a442442d4fac9fe687e0f1f3af0625f7fc35510
Arthur <65594296+InTheDarkAlone@users.noreply.github.com>
2020-11-11 12:44:00 +0500

    Full Task 7

---
+++ Task/Task/SLList.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+class SLList
+{
+private:
+	int m_data;
+public:
+	SLList* m_nextElem;
+	int getData() const;
+	void setData(int data);
+	SLList* next() const;
+	bool hasNext() const;
+	static SLList* beginList(int data);
+	SLList* addElement(int data);
+	void removeNext();
+	SLList* FindByIndex(int index);
+};
