3eee070b29324aa77faf4e7d1e3e7164eda3e988
Arthur <65594296+InTheDarkAlone@users.noreply.github.com>
2020-11-27 16:45:15 +0500

    Task 8 with added fixes

---
+++ Task/MCell.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+class MCell
+{
+private:
+	bool m_down = false;
+	bool m_right = false;
+public:
+	bool get_right() const;
+	bool get_down() const;
+	friend class Maze;
+};
---
+++ Task/Task_plus.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "Maze.h"
+
+
+int main()
+{
+	Maze* maze = new Maze(12, 12);
+	int step = 1;
+	int x = maze->get_length() - 1;
+	int y = maze->get_width();
+	y = 0;
+	x = 1;
+	while (true)
+	{
+		int i = y;
+		for (; i < y + step; i++)
+			maze->makeConnection(x, i, x, i + 1);
+		if (y == maze->get_width())
+			break;
+		y = i;
+		i = x;
+		for (; i < x + step; i++)
+			maze->makeConnection(i, y, i + 1, y);
+		x = i;
+		if (x == maze->get_length())
+			break;
+	}
+
+	maze->printMaze();
+}
