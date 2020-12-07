f92539ddc75f4bead4cdba8b77a7cb6d5c95648e
Arthur <65594296+InTheDarkAlone@users.noreply.github.com>
2020-12-07 19:02:30 +0500

    Full 9 task

---
+++ MTreeNode.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+class MTreeNode
+{
+private:
+	int m_i;
+	int m_j;
+	MTreeNode* m_parent;
+	MTreeNode** m_children;
+	int m_distance;
+public:
+	MTreeNode(MTreeNode* parent);
+	int get_i() const;
+	int get_j() const;
+	const MTreeNode* get_parent() const;
+	MTreeNode* get_child(int i) const;
+	int get_distance() const;
+	int childCount() const;
+	bool addChild(int i, int j);
+	MTreeNode* hasChild(int i, int j);
+	MTreeNode* beginTree(int i, int j);
+	friend class Maze;
+};
+
---
+++ Maze.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "MCell.h"
+
+class Maze
+{
+private:
+	int n = 0;
+	int m = 0;
+	MCell* maze;
+public:
+	Maze(int n, int m);
+	const MCell cell(int i, int j) const;
+	bool hasConnection(int i1, int j1, int i2, int j2);
+	bool makeConnection(int i1, int j1, int i2, int j2);
+	void removeConnection(int i1, int j1, int i2, int j2);
+	void printMaze();
+	int get_length();
+	int get_width();
+};
+
